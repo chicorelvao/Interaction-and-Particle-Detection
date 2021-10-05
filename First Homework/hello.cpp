347daac17961db71b54a6ceaf4ad13f96e3a14f2
Francsico Relvão <franciscofrelvao@gmail.com>
2021-10-05 13:10:01 +0100

    Small comments

--- First Homework/hello.cpp
+++ First Homework/hello.cpp
@@ -25,21 +25,12 @@
 
 
 #include <iostream>
-
 #include <fstream>
-
 #include <stdlib.h>
-
 #include <time.h>
-
-
-
 #include <math.h>
 
-
-
 using namespace std;
-
 
 
 int main(){
@@ -48,11 +39,8 @@ int main(){
 
 	cout << "This is how you write to screen in C++" << endl;
 
-
-
 	ofstream outFile("random_numbers.dat");
 
-	
 
 	// First of all we must initialise the random generator with a seed
 
@@ -77,8 +65,9 @@ int main(){
 	cout << "Number of random numbers: " << N << endl;
 
 
-
+	//An array is not necessary, but it keeps things cleans
 	double randomArr[N];
+	//Pick a lambda to change the "balance" of the distribution
 	double lambda = 2.1;	
 
 	// Now let's do a cycle and write randoms to the screen and/or file
@@ -95,9 +84,11 @@ int main(){
 
 		// end up with thousands (millions) of lines in the terminal
 
+		//We are doing the exponential distribution
+
 		randomArr[i] = -1.0*(1.0/lambda)*log(1.0-random);
 
-
+		//Store each array element in the file
 		outFile << randomArr[i] << endl;
 
 	}
